18c2014cd09153e9eb1424f7f20a8834e02b57b1
cocomong11 <carpediem1011@naver.com>
2025-11-30 17:24:21 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct student{
+	int ID;
+	char name[20];
+	double grade;
+	};
+int main(int argc, char *argv[]) {
+	struct student stud={10,"Chaehyeon",4.3};
+	struct student *stdPtr;
+	stdPtr=&stud;
+	stdPtr->ID=17;
+	strcpy(stdPtr->name,"PARK");
+	stdPtr->grade=2.0;
+	
+	
+	printf("ID:%i,name:%s,grade:%lf\n",stdPtr->ID,stdPtr->name,stdPtr->grade);
+	system("PAUSE");
+
+	return 0;
+}
